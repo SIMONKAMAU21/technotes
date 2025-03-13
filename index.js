24e9bd3ecb4378a28dd4054860a6d029745b843d
simon <simogatuma21@gmail.com>
2025-03-13 20:55:47 +0300

    fixed the origin logs

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
-      console.log(origin)
     } else {
       callback(new Error("Not allowed by CORS"));
     }
